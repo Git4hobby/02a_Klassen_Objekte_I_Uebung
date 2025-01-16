f86b275da1ee4b7b3a0eca0d28d6d401080bcc7b
CCHobbit <Markus_Clephas@t-online.de>
2025-01-16 11:08:13 +0100

    Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,20 @@ public class App {
     
     public static void main(String[] args) {
 
-
+        /*
+         * Instanzierung --> Erzeugen eines/mehrer Objekte
+         * Objekt <---> Instanz
+         * cat --> Referenzvariable
+         * cat --> Refernztyp(Klasse)
+         * 
+         */
+    
+         Cat cat = new Cat();
+        System.out.println(cat);
+        
     }
 
-   
+
 
 }
 
